210351c493af368e66d9bf8a49700dd6b558af7c
zampa00 <zampa92@gmail.com>
2022-03-06 07:58:39 -0800

    extract rules

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,22 @@
 defmodule FizzBuzz do
+
   @spec say(integer) :: String.t()
   def say(number) do
-    rules = [
-      whenDivThenSay(3, "Fizz"),
-      whenDivThenSay(5, "Buzz")
-    ]
-
-    number
-    |> apply_rules(rules)
+    get_rules()
+    |> apply_rules(number)
     |> apply_default_rule(number)
   end
 
-  @spec apply_rules(integer, list((integer -> String.t()))) :: String.t()
-  def apply_rules(number, rules) do
+  @spec get_rules() :: list((integer -> String.t()))
+  def get_rules() do
+    [
+      whenDivThenSay(3, "Fizz"),
+      whenDivThenSay(5, "Buzz")
+    ]
+  end
+
+  @spec apply_rules(list((integer -> String.t())), integer) :: String.t()
+  def apply_rules(rules, number) do
     Enum.reduce(rules, "", fn rule, acc -> "#{acc}#{rule.(number)}" end)
   end
 
