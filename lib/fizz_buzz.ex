45d321331c149854346cb7c3f60b4b777c86b27f
zampa00 <zampa92@gmail.com>
2022-03-06 07:30:54 -0800

    first refactor to extract rules

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,29 @@
 defmodule FizzBuzz do
   @spec say(integer) :: String.t()
   def say(number) do
-    if rem(number, 15) == 0 do
-      "FizzBuzz"
-    else
-      if rem(number, 3) == 0 do
-        "Fizz"
-      else
-        if rem(number, 5) == 0 do
+    rules = [
+      fn x ->
+        if rem(x, 3) == 0 do
+          "Fizz"
+        else
+          ""
+        end
+      end,
+      fn x ->
+        if rem(x, 5) == 0 do
           "Buzz"
         else
-          "#{number}"
+          ""
         end
       end
+    ]
+
+    result = Enum.reduce(rules, "", fn rule, acc -> "#{acc}#{rule.(number)}" end)
+
+    if result == "" do
+      "#{number}"
+    else
+      result
     end
   end
 end
